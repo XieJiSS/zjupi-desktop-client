c0727d082e336d28d07d65260152b9a60fa8b915
XieJiSS <XieJiSS@users.noreply.github.com>
2022-09-07 16:43:06 +0800

    fix: match realworld scenario

--- install.bat
+++ install.bat
@@ -1,11 +1,10 @@
 @echo off
 
-powershell -Command "echo Unrestricted" || regedit.exe /S %cd%\ps-policy.reg
-powershell -Command "pm2 --version" || goto install
-exit
+chcp 65001
+powershell -Command "pm2 --version" && exit
 
-:install
 echo "上述报错表明 pm2 尚未安装。尝试安装 pm2……"
+regedit.exe /S %cd%\ps-policy.reg
 npm i -g pm2 --registry=https://registry.npmmirror.com
 echo "安装成功。"
 pause
