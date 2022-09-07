b19c70af872fc8d5223826a1126fac32bb7de5c3
XieJiSS <XieJiSS@users.noreply.github.com>
2022-09-07 16:32:14 +0800

    chore: better installation guide

--- install.bat
+++ install.bat
@@ -1,2 +1,11 @@
-regedit.exe /S %cd%\ps-policy.reg
+@echo off
+
+powershell -Command "echo Unrestricted" || regedit.exe /S %cd%\ps-policy.reg
+powershell -Command "pm2 --version" || goto install
+exit
+
+:install
+echo "上述报错表明 pm2 尚未安装。尝试安装 pm2……"
 npm i -g pm2 --registry=https://registry.npmmirror.com
+echo "安装成功。"
+pause
