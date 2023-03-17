a063fa37e97ec1dfe92829629dc7e3c5b1509830
XieJiSS <XieJiSS@users.noreply.github.com>
2023-03-17 16:24:49 +0800

    chore: bump version

--- install.bat
+++ install.bat
@@ -9,9 +9,11 @@ regedit.exe /S %cd%\ps-policy.reg
 npm i --location=global yarn
 yarn config set registry https://registry.npm.taobao.org
 yarn global add pm2
-echo "安装成功。"
+yarn install
+echo "done."
 
 :task
+npm i --location=global yarn
 yarn
 node ./start-up.js
 
