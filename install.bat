db14217a08cc57e2b02db5f2ccdd48050ddc549a
XieJiSS <XieJiSS@users.noreply.github.com>
2023-03-17 20:39:10 +0800

    fix: pwsh failed to find yarn

--- install.bat
+++ install.bat
@@ -6,14 +6,14 @@ powershell -Command "pm2 --version" && goto :task
 echo "The above error shows that pm2 is not installed."
 echo "Installing pm2..."
 regedit.exe /S %cd%\ps-policy.reg
-powershell -Command "npm i --location=global yarn"
+powershell -Command "npm i -g yarn"
 powershell -Command "yarn config set registry https://registry.npm.taobao.org"
 powershell -Command "yarn global add pm2"
 powershell -Command "yarn install"
 echo "done."
 
 :task
-powershell -Command "npm i --location=global yarn"
+powershell -Command "npm i -g yarn"
 powershell -Command "yarn"
 node ./start-up.js
 
