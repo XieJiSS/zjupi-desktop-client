6e1446340c80ab21f9027745ce567e6d38de349d
XieJiSS <XieJiSS@users.noreply.github.com>
2023-03-17 21:44:18 +0800

    fix: yarn global bin not in PATH

--- install.bat
+++ install.bat
@@ -6,9 +6,8 @@ powershell -Command "pm2 --version" && goto :task
 echo "The above error shows that pm2 is not installed."
 echo "Installing pm2..."
 regedit.exe /S %cd%\ps-policy.reg
-powershell -Command "npm i -g yarn"
+powershell -Command "npm i -g yarn pm2"
 powershell -Command "yarn config set registry https://registry.npm.taobao.org"
-powershell -Command "yarn global add pm2"
 powershell -Command "yarn install"
 echo "done."
 
